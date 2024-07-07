8a383fcf36f5279f0cdfaf0b6788b345a1705486
Boris Medvedev <borian.medvedev@gmail.com>
2024-07-08 01:15:58 +0200

    Change index php logic

--- index.php
+++ index.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 use Kernel\Kernel;
 
-require_once './vendor/autoload.php';
-
+$autoloadPath = __DIR__ . '/vendor/autoload.php';
+if (!file_exists($autoloadPath)) {
+    throw new \RuntimeException('Autoload file not found. Please run "composer install".');
+}
+require_once $autoloadPath;
 
 (new Kernel())->bootstrap();
